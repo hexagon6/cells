f1177ae61b24cbf75e9ef5a8507e8ef951b1390e
Hexagon Six <hexagon6@fet.li>
2022-05-07 17:07:57 +0300

    cleanup: remove /html endpoint

--- cell.js
+++ cell.js
@@ -24,23 +24,6 @@ async function handleRequest(request) {
 
   const { pathname } = new URL(request.url)
 
-  if (pathname.startsWith('/html')) {
-    const html = `<html>
-      <p><b>Message:</b> Hello from Deno Deploy.</p>
-      </html>`
-
-    return new Response(html, {
-      headers: {
-        // The interpretation of the body of the response by the client depends
-        // on the 'content-type' header.
-        // The "text/html" part implies to the client that the content is HTML
-        // and the "charset=UTF-8" part implies to the client that the content
-        // is encoded using UTF-8.
-        'content-type': 'text/html; charset=UTF-8',
-      },
-    })
-  }
-
   if (pathname.startsWith('/world')) {
     const [, , dim] = pathname.split('/')
     const [x, y] = dim.split('x')
